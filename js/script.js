317e0aa32ce376b1180e334ff4ed88af7b366dc5
Oleh <eyeofatiger@yandex.ua>
2019-09-15 14:18:43 +0100

    Add modal window for Ask more

--- js/script.js
+++ js/script.js
@@ -83,4 +83,34 @@ window.addEventListener("DOMContentLoaded", function(){
         }
 
         setClock("timer", deadLine);
+
+    // Modal
+
+    let more = document.querySelector(".more"),
+        overlay = document.querySelector(".overlay"),
+        close = document.querySelector(".popup-close");
+
+        more.addEventListener("click", function(){
+            overlay.style.display = "block";
+            this.classList.add("more-solash");
+            document.body.style.overflow = "hidden";
+        });
+
+        close.addEventListener("click", function(){
+            overlay.style.display = "none";
+            more.classList.remove("more-solash");
+            document.body.style.overflow = "";
+        });
+    
+    //Modal for tabs
+
+    let desBtn = document.querySelectorAll(".description-btn");
+        for(let i = 0; i<desBtn.length; i++){
+        desBtn[i].addEventListener("click", function(){
+        overlay.style.display = "block";
+        this.classList.add("more-solash");
+        document.body.style.overflow = "hidden";
+    });
+}
+
 });
