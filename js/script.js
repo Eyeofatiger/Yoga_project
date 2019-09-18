2b75c004e9e4f15c0b69e5c11626a89719534388
Oleh <eyeofatiger@yandex.ua>
2019-09-18 23:48:16 +0100

    Add slider

--- js/script.js
+++ js/script.js
@@ -113,7 +113,7 @@ window.addEventListener("DOMContentLoaded", function(){
     });
 }
 
-    // Form + FormData + main-form
+    // Form + FormData
 
     /* let massage = {
         loading: "Loading...",
@@ -204,4 +204,51 @@ window.addEventListener("DOMContentLoaded", function(){
             });
         }
 
+        // Slider
+
+        let slideIndex = 1,
+            slides = document.querySelectorAll(".slider-item"),
+            prev = document.querySelector(".prev"),
+            next = document.querySelector(".next"),
+            dotsWrap = document.querySelector(".slider-dots"),
+            dots = document.querySelectorAll(".dot");
+        
+        function showSlides(n){
+
+            if(n>slides.length){
+                slideIndex=1;
+            }
+            if(n<1){
+                slideIndex = slides.length;
+            }
+
+            slides.forEach((item) => item.style.display = "none");
+            dots.forEach((dot)=>dot.classList.remove("dot-active"));
+
+            slides[slideIndex-1].style.display = "block";
+            dots[slideIndex-1].classList.add("dot-active");
+        }
+
+        function plusSlide(n){
+            showSlides(slideIndex +=n);
+        }
+        function currentSlide(n){
+            showSlides(slideIndex = n);
+        }
+
+        prev.addEventListener("click", function(){
+            plusSlide(-1);
+        });
+
+        next.addEventListener("click", function(){
+            plusSlide(1);
+        });
+
+        dotsWrap.addEventListener("click", function(event){
+            for (let i=0; i<dots.length+1; i++){
+                if(event.target.classList.contains("dot") && event.target == dots[i-1]){
+                    currentSlide(i);
+                }
+            }
+        });
 });
