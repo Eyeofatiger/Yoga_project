d0c9653dbdcc93833271bac8e1c096db48aeaef9
Oleh <eyeofatiger@yandex.ua>
2019-09-19 11:02:10 +0100

    Add calculator with price

--- js/script.js
+++ js/script.js
@@ -251,4 +251,46 @@ window.addEventListener("DOMContentLoaded", function(){
                 }
             }
         });
+
+        //Calc
+
+        let persons = document.querySelectorAll(".counter-block-input")[0],
+            restDays = document.querySelectorAll(".counter-block-input")[1],
+            place = document.getElementById("select"),
+            totalValue = document.getElementById("total"),
+            personSum = 0,
+            daySum = 0,
+            total = 0;
+
+        totalValue.innerHTML = 0;
+
+        persons.addEventListener("change", function(){
+            personSum = +this.value;
+            total = daySum*personSum*100;
+
+            if(restDays.value ==""){
+                totalValue.innerHTML = 0;
+            } else{
+                totalValue.innerHTML = total;
+            }
+        });
+
+        restDays.addEventListener("change", function(){
+            daySum = +this.value;
+            total = daySum*personSum*100;
+
+            if(persons.value ==""){
+                totalValue.innerHTML = 0;
+            } else{
+                totalValue.innerHTML = total;
+            }
+        });
+
+        place.addEventListener("change", function(){
+            if(restDays.value == "" || persons.value == ""){
+                totalValue.innerHTML = 0;
+            } else {
+                totalValue.innerHTML = total * +this.value;
+            }
+        });
 });
